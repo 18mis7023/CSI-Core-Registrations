c35de686b7976bf14bbbedd678698ff51736a1d6
18mis7023 <hemanth.18mis7023@vitap.ac.in>
2021-08-08 15:42:11 +0530

    basic Errors fixed

--- src/App.js
+++ src/App.js
@@ -3,37 +3,18 @@ import RegisterStage1 from './Components/RegisterStage1';
 import RegisterStage2 from './Components/RegisterStage2';
 import Header from './Components/Header';
 import HomeContent from './Components/HomeContent';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import ScrollToTop from "./Hooks/ScrollToTop";
+import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      {/* <Header />
+      <Header />
       <Router>
           <Switch>
               <Route exact path="/" component={HomeContent}></Route>
               <Route exact path="/registerstage1" component={RegisterStage1}></Route>
               <Route exact path="/registerstage2" component={RegisterStage2}></Route>
           </Switch>
-        </Router> */}
-        <BrowserRouter>
-        <Header />
-        <ScrollToTop>
-          <div className="app_child">
-            <Switch>
-              <Route exact path="/">
-                <HomeContent />
-              </Route>
-              <Route path="/registerstage1">
-                <RegisterStage1 />
-              </Route>
-              <Route path="/registerstage1">
-                <RegisterStage2 />
-              </Route>
-            </Switch>
-          </div>
-        </ScrollToTop>
-      </BrowserRouter>
+        </Router>
     </div>
   );
 }
