d04eb01c8146cb2241d04daacab1e1f372430062
Sudhee-bsp <sudheendrapai.b@gmail.com>
2021-08-12 19:14:50 +0530

    logout added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import './App.css';
-import RegisterStage1 from './Components/RegisterStage1';
-import RegisterStage2 from './Components/RegisterStage2';
-import Header from './Components/Header';
-import HomeContent from './Components/HomeContent';
-import {BrowserRouter as Router,Route,Switch} from "react-router-dom";
+import "./App.css";
+import RegisterStage1 from "./Components/RegisterStage1";
+import RegisterStage2 from "./Components/RegisterStage2";
+import Header from "./Components/Header";
+import HomeContent from "./Components/HomeContent";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import RegisterStage3 from './Components/RegisterStage3';
-import RegisterStage3 from './Components/RegisterStage3';
+import RegisterStage3 from "./Components/RegisterStage3";
 function App() {
   return (
     <div className="App">
       <Header />
       <Router>
-          <Switch>
-              <Route exact path="/" component={HomeContent}></Route>
-              <Route exact path="/registerstage1" component={RegisterStage3}></Route>
-              <Route exact path="/registerstage2" component={RegisterStage2}></Route>
-              {/* <Route exact path="/registerstage3" component={RegisterStage3}></Route> */}
-
-          </Switch>
-        </Router>
+        <Switch>
+          <Route exact path="/" component={HomeContent}></Route>
+          <Route
+            exact
+            path="/registerstage1"
+            component={RegisterStage1}
+          ></Route>
+          <Route
+            exact
+            path="/registerstage2"
+            component={RegisterStage2}
+          ></Route>
+          <Route
+            exact
+            path="/registerstage3"
+            component={RegisterStage3}
+          ></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,42 +1,98 @@
-import React from 'react'
-import logo from "../assets/images/logo.png"
-import "../assets/css/header.css"
-import firebase from "../firebaseauth.js"
-import {Redirect} from "react-router-dom"
-class Header extends React.Component{
-    
-    logout() {
-        firebase.auth.signOut().then((result) => {
-            console.log("hello")
-        })
+import React from "react";
+import logo from "../assets/images/logo.png";
+import "../assets/css/header.css";
+import firebase from "../firebaseauth.js";
+import { Redirect } from "react-router-dom";
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+    };
+  }
+
+  logout() {
+    firebase
+      .auth()
+      .signOut()
+      .then(function () {
+        this.setState({
+          user: null,
+        });
+        console.log("Logged-out successfully");
+      })
+      .catch(function (error) {
+        console.log("error while logging out");
+      });
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
       }
-    render(){
-        return(
-            <div>
-            <nav className="navbar navbar-default navbar-fixed-top">
-                <div className="container">
-                    <div className="navbar-header">
-                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>                        
-                        </button>
-                        <a className="navbar-brand" id="navbar-brand" href="./">
-                        <img className="logo" src={logo} alt="CSI VITAP"/> </a>
-                    </div>
-                    <div className="collapse navbar-collapse" id="myNavbar">
-                        <ul className="nav navbar-nav navbar-right">
-                            <li><a href = "./"><h5 className="tag">HOME</h5></a></li>
-                            <li><a href = "http://csivitap.co.in/"><h5 className="tag">WEBSITE</h5></a></li>
-                            <li><a href = "./"><h5 className="tag">CONTACT US</h5></a></li>
-                            <li><a onClick= {this.logout}><h5 className="tag">LOGOUT</h5></a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+    });
+  }
+
+  render() {
+    let authButton = this.state.user ? (
+      <button onClick={this.logout}>
+        <h5 className="tag">LOGOUT</h5>
+      </button>
+    ) : (
+      <button>
+        <h5 className="tag">REGISTER</h5>
+      </button>
+    );
+
+    return (
+      <div>
+        <nav className="navbar navbar-default navbar-fixed-top">
+          <div className="container">
+            <div className="navbar-header">
+              <button
+                type="button"
+                className="navbar-toggle"
+                data-toggle="collapse"
+                data-target="#myNavbar"
+              >
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" id="navbar-brand" href="./">
+                <img className="logo" src={logo} alt="CSI VITAP" />{" "}
+              </a>
             </div>
-        );
-    }
+            <div className="collapse navbar-collapse" id="myNavbar">
+              <ul className="nav navbar-nav navbar-right">
+                <li>
+                  <a href="./">
+                    <h5 className="tag">HOME</h5>
+                  </a>
+                </li>
+                <li>
+                  <a href="http://csivitap.co.in/">
+                    <h5 className="tag">WEBSITE</h5>
+                  </a>
+                </li>
+                <li>
+                  <a href="./">
+                    <h5 className="tag">CONTACT US</h5>
+                  </a>
+                </li>
+                <li>
+                  <a>{authButton}</a>
+                  {/* <button onClick={() => this.logout()}>Log Out</button> */}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default Header;
